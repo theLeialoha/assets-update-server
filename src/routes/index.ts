1fc0d62d8c1850b50f41433afe7bf6485f3b4c0d
Leialoha <github@leialoha.dev>
2025-06-15 21:25:52 -0500

    Possible fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import { validate as validateUUID } from 'uuid';
 export * from './ApiKeys';
 export * from './Mods';
 
-export const validateApiKey = asyncRoute(async function (req: Request, res: Response, next: NextFunction) {
+export async function validateApiKey(req: Request, res: Response, next: NextFunction) {
     const { mod } = req.params;
     
     const apiKey: string = req.headers['apikey'] as any;
@@ -19,16 +19,16 @@ export const validateApiKey = asyncRoute(async function (req: Request, res: Resp
     if (result?.mods?.includes('*')) return next();
     else if (result?.mods?.includes(mod.toLowerCase())) return next();
     throw new ExpressError(403, 'Unauthorized');
-});
+};
 
-export const validateMasterApiKey = asyncRoute(async function (req: Request, res: Response, next: NextFunction) {
+export async function validateMasterApiKey(req: Request, res: Response, next: NextFunction) {
     const apiKey: string = req.headers['apikey'] as any;
     if (!validateUUID(apiKey)) throw new ExpressError(401, 'API Key not provided');
     else if (MASTER_KEY?.toLowerCase() != apiKey.toLowerCase()) throw new ExpressError(403, 'Unauthorized');
     next();
-});
+};
 
-export function asyncRoute(handler: RequestHandler) {
+export function asyncRoute(handler: RequestHandler): RequestHandler {
     return (req: Request, res: Response, next: NextFunction) => {
         // Weird catch system... :/
         Promise.resolve(handler(req, res, next)).catch(e => next(e));
