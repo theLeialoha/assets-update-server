264b63218cab38946764b232e752a012781e7b2c
Leialoha <github@leialoha.dev>
2025-06-15 21:12:32 -0500

    Forgot to remove old import (again)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,11 @@ import { ApiKeyModel } from '../database';
 import { MASTER_KEY } from '../constants';
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { validate as validateUUID } from 'uuid';
-const asyncHandler = require("express-async-handler");
 
 export * from './ApiKeys';
 export * from './Mods';
 
-export const validateApiKey = asyncHandler(async function (req: Request, res: Response, next: NextFunction) {
+export const validateApiKey = asyncRoute(async function (req: Request, res: Response, next: NextFunction) {
     const { mod } = req.params;
     
     const apiKey: string = req.headers['apikey'] as any;
@@ -22,7 +21,7 @@ export const validateApiKey = asyncHandler(async function (req: Request, res: Re
     throw new ExpressError(403, 'Unauthorized');
 });
 
-export const validateMasterApiKey = asyncHandler(async function (req: Request, res: Response, next: NextFunction) {
+export const validateMasterApiKey = asyncRoute(async function (req: Request, res: Response, next: NextFunction) {
     const apiKey: string = req.headers['apikey'] as any;
     if (!validateUUID(apiKey)) throw new ExpressError(401, 'API Key not provided');
     else if (MASTER_KEY?.toLowerCase() != apiKey.toLowerCase()) throw new ExpressError(403, 'Unauthorized');
